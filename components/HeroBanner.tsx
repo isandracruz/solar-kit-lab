225eccd1f75d8ff469c4639e79dd887556d36480
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-02-23 16:47:42 -0500

    feat: add HeroBanner component

---
+++ components/HeroBanner.tsx
@@ -0,0 +1,14 @@
+function HeroBanner() {
+    return (
+        <section className="mb-8 ">
+            <div className="mb-8 flex flex-col items-center gap-2 w-[80%] mx-auto text-center">
+                <h2 className="text-3xl font-bold text-primary mb-2">Diseña tu sistema solar</h2>
+                <p className="text-muted-foreground">
+                    Descubre exactamente qué equipos solares necesitas. Añade tus electrodomésticos y mira cómo se calculan tus requerimientos en tiempo real. Sin lenguaje técnico complejo.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default HeroBanner;
