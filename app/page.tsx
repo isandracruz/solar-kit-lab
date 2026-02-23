05c4de80133f803e229571748edb045c7e74bd21
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-02-23 16:48:55 -0500

    feat: replace welcome message with HeroBanner component in Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import HeroBanner from '@/components/HeroBanner';
 import Layout from '@/components/layout/Layout';
 
 export default function Page() {
   return (
     <Layout>
-      <h1 className="text-4xl font-bold text-center mt-20">Bienvenido a Solar Kit Lab</h1>
+      <HeroBanner />
     </Layout>
   );
 }
