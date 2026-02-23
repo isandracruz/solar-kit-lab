4dda58643b1dd40ba17e314bd8ad0f5047280b37
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-02-23 16:48:29 -0500

    feat: restructure Layout component to improve child element styling

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,7 +6,13 @@ function Layout({ children }: { children: React.ReactNode }) {
         <main className="min-h-screen bg-background">
             <div className="flex flex-col min-h-screen">
                 <Header />
-                <main className="grow">{children}</main>
+
+                <section className="grow">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                        {children}
+                    </div>
+                </section>
+
                 <Footer />
             </div>
         </main>
