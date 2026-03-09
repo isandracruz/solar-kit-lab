e37e3d972614e1bbfc7f1fbe624b2ecf1518329a
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-03-09 17:44:51 -0400

    fix: add suppressHydrationWarning to body element in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning >
         {children}
         <Analytics />
       </body>
